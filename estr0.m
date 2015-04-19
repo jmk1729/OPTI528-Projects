c1a65f3ef4f451a2bde78c193d7de998c69804b0
jmk1729 <jmk1729@email.arizona.edu>
2015-04-18 19:28:04 -0700

    Changed WindSpeed.m to estr0.m for employing the MATLAB integral function. Modified HVModel.m slightly since rms_wind_speed should only be a scalar. rmsWindSpeed.m is a test function for trapezoidal integration with trapz.

---
+++ estr0.m
@@ -0,0 +1,46 @@
+function [windSpeed, Vrms, r0] = estr0(month,altitude,rmsFlag)
+% Outputs the wind speed for the input month (1-12) and list of altitudes,
+% option for rms wind speed and r0 estimation
+
+%% Initializations
+Tucson = load('Tucson.mat'); % Load table data from Optics Express, Vol. 19, Issue 2, pp. 820-837 (2011) http://dx.doi.org/10.1364/OE.19.000820
+AConst = Tucson.a;
+ACMonth = AConst(month,1:4);
+LowAlt = 2000; % meters
+HighAlt = 7000; % meters
+k = 2*pi/(700e-9); % meters
+%% Wind Speed Calculation
+% Start with non-linear least squares data fit, this should be a Gaussian
+A0 = ACMonth(1);
+A1 = ACMonth(2);
+A2 = ACMonth(3);
+A3 = ACMonth(4);
+
+windSpeed = A0 + A1*exp(-((altitude-A2)/A3).^2); % Equation (3) in the reference paper, altitude in meters
+
+% Sanity check
+% figure;
+% plot(windSpeed, altitude);
+% xlabel('Wind Speed (m/s)');
+% ylabel('Altitude from mean sea level (km)');
+% title('Wind Profile');
+
+%% Integrate for RMS Wind Speed
+if rmsFlag == true
+    Vsq = @(z)(A0 + A1*exp(-((z-A2)/A3).^2)).^2;
+    Vrms = sqrt((1/15000)*integral(Vsq,LowAlt,HighAlt)); %Dyson Principles of AO 3rd Ed. equation (2.16)
+else
+    Vrms = 'Not Computed';
+end
+
+%% r0 Calculation
+% Fried coherence length calculated using the rms wind speed parameter.
+if rmsFlag == true
+    A = 1.7e-14; % Cn2 at ground level, published value referenced in HVModel ref 2)
+    Cn2 = @(h) 0.00594*((Vrms/27).^2).*(((10^-5).*h).^10).*exp(-h./1000) ...
+        +(2.7*10^-16)*exp(-h./1500) + A.*exp(-h./100); % HVModel
+    r0 = (0.423*k^2*integral(Cn2,0,HighAlt)).^(-3/5); % Dyson
+else
+    r0 = 'Not Computed';
+end
+end
