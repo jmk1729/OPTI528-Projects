f7d69b0b95c9d7d2a289c2c31288e77db650a58c
jmk1729 <jmk1729@email.arizona.edu>
2015-04-24 14:41:46 -0700

    Calculation of r0 for spherical wave.

--- estr0.m
+++ estr0.m
@@ -1,4 +1,4 @@
-function [windSpeed, Vrms, r0] = estr0(month,altitude,rmsFlag)
+function [windSpeed, Vrms, r0] = estr0(month,rmsFlag,zenAngle,altitude)
 % Outputs the wind speed for the input month (1-12) and list of altitudes,
 % option for rms wind speed and r0 estimation
 
@@ -6,7 +6,7 @@ function [windSpeed, Vrms, r0] = estr0(month,altitude,rmsFlag)
 Tucson = load('Tucson.mat'); % Load table data from Optics Express, Vol. 19, Issue 2, pp. 820-837 (2011) http://dx.doi.org/10.1364/OE.19.000820
 AConst = Tucson.a;
 ACMonth = AConst(month,1:4);
-LowAlt = 1000; % meters
+LowAlt = 0; % meters
 HighAlt = 7000; % meters
 lambda = AOField.VBAND;
 k = 2*pi/lambda; % meters
@@ -38,16 +38,16 @@ end
 % Fried coherence length calculated using the rms wind speed parameter.
 if rmsFlag == true
     A = 1.7e-14; % Cn2 at ground level, published value referenced in HVModel ref 2)
-    Cn2 = @(h) 0.00594*((Vrms/27).^2).*(((10^-5).*h).^10).*exp(-h./1000) ...
-        +(2.7*10^-16)*exp(-h./1500) + A.*exp(-h./100); % HVModel
+    Cn2 = @(h) (0.00594*((Vrms/27).^2).*(((10^-5).*h).^10).*exp(-h./1000) ...
+        +(2.7*10^-16)*exp(-h./1500) + A.*exp(-h./100)).*(h/HighAlt).^(5/3); % HVModel
     
-    figure(2)
-    fplot(Cn2,[LowAlt,HighAlt]);
-    xlabel('Altitude in meters');
-    ylabel('H-V Computed C_n^2');
-    title('C_n^2 vs Altitude for Tucson');
+%     figure(2)
+%     fplot(Cn2,[LowAlt,HighAlt]);
+%     xlabel('Altitude in meters');
+%     ylabel('H-V Computed C_n^2');
+%     title('C_n^2 vs Altitude for Tucson');
     
-    r0 = (0.423*k^2*integral(Cn2,LowAlt,HighAlt)).^(-3/5); % Dyson
+    r0 = (0.423*k^2*secd(zenAngle)*integral(Cn2,LowAlt,HighAlt)).^(-3/5); % Dyson
 else
     r0 = 'Not Computed';
 end
